88a1118e111061a430b3796b9f1e789b8b935b51
dom-VMS <dom.aracri@varland.com>
2018-11-13 08:55:12 -0500

    Lots of Front-end changes with instances of errors.
    More bootstrap work has been done to make the app look and function well. Currently, an error exsists in which a user tries to who wants to view other departments tasks but can't. Pushing this all now before I attempt to install and make use of a new Gem.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,6 @@
 class HomeController < ApplicationController
   def index
     if logged_in?
-      user_group = current_user.user_groups
-      @task_types = []
-      user_group.each do |ug| 
-        @task_types.push(TaskType.find_by_id(ug.task_type_option.task_type_id))
-      end
     else
       render partial: 'errors/not_signed_in'
     end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,11 +4,12 @@ class TasksController < ApplicationController
     http_basic_authenticate_with name: "admin", password: "Vms.1946!", only: :destroy
 
     def index
-      user_group = current_user.user_groups
-      @task_types = []
-      user_group.each do |ug| 
-        @task_types.push(TaskType.find_by_id(ug.task_type_option.task_type_id))
-      end
+        @task = Task.all
+      #user_group = current_user.user_groups
+      #@task_types = []
+      #user_group.each do |ug| 
+      #  @task_types.push(TaskType.find_by_id(ug.task_type_option.task_type_id))
+      #end
     end
     
     def show
