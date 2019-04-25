6ffb8d64defa9752325cce4bec150770894a434b
dom-VMS <dom.aracri@varland.com>
2019-04-25 15:02:05 -0400

    Refactored the process in tasks/_assigned_to_user
    Instead of:
    
    Finding all the task_types a user belongs to X all tasks asisgned to user = match task_type to task
    
    Now: all tasks assigned to user x task_types of tasks = match.
    
    The process is much faster and more efficient.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,8 @@ class HomeController < ApplicationController
   skip_before_action :require_login, only: [:ticket, :welcome]
 
   def index
-    task_type_ids = TaskType.get_task_types_assigned_to_user(current_user) 
-    @task_types = TaskType.find(task_type_ids) 
-
-    @task_types.each do |task_type|
-      if task_type.children.any?
-        task_type.children.each do |child|
-          @task_types.append(child) unless (@task_types.any? {|task_type| task_type == child})
-        end
-      end
-    end
     @task = Task.get_all_tasks_assigned_to_user(current_user)
+    @task_types = TaskType.where(id: @task.pluck(:task_type_id).uniq).order(:parent_id)
   end
 
   def ticket
