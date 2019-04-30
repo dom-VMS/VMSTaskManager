9f57a6905a4a541830332208cc4f3e59ffbd09a5
dom-VMS <dom.aracri@varland.com>
2019-04-30 09:44:09 -0400

    Minor Changes to Activities and Comments
    - Activities are now ordered in a much cleaner manner.
    - Validation for comments exsists.
    - Error messages should appear for comments.

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,8 +1,8 @@
 module ActivitiesHelper
     # Used to track activities on a specific task.
     def self.get_activities(task)
-        activities_by_trackable = PublicActivity::Activity.order("created_at desc").where("trackable_type = ? and trackable_id = ?", "Task", task.id)
-        activities_by_recipient = PublicActivity::Activity.order("created_at desc").where("recipient_type = ? and recipient_id = ?", "Task", task.id)
-        return activities = activities_by_trackable + activities_by_recipient
+        activities_by_trackable = PublicActivity::Activity.where("trackable_type = ? and trackable_id = ?", "Task", task.id)
+        activities_by_recipient = PublicActivity::Activity.where("recipient_type = ? and recipient_id = ?", "Task", task.id)
+        return activities = (activities_by_trackable + activities_by_recipient).sort_by{|activity| activity.created_at}
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,8 @@ class Comment < ApplicationRecord
   belongs_to :task
   has_many :file_attachments, dependent: :destroy
 
+  validates_presence_of :body, :message => "of the comment cannot be empty."
+
   tracked owner: Proc.new{ |controller, model| controller.current_user }
   tracked  recipient: ->(controller, model) { model && model.task }
 end
